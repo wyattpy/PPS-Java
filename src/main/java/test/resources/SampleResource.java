387ed8b9456cd2e914b9b9be0413e12dff250b0f
wyattpy <71204725+wyattpy@users.noreply.github.com>
2020-10-28 21:19:35 -0300

    aa

--- src/main/java/test/resources/SampleResource.java
+++ src/main/java/test/resources/SampleResource.java
@@ -1,63 +1,29 @@
 package test.resources;
 
-import com.google.common.io.Resources;
-
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
+import org.glassfish.jersey.server.mvc.Viewable;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class SampleResource {
 
     @GET
-    @Path("/")
-    @Produces(MediaType.TEXT_HTML)
-    public String showHellowWorld() {
-        String content = "";
-        try {
-            URL url = Resources.getResource("index.html");
-            content = Resources.toString(url, StandardCharsets.UTF_8);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-        return content;
+    @Path("")
+    public Viewable index() {
+        return new Viewable("/index.html");
     }
 
     @GET
     @Path("/login")
-    public String showLogin() {
-        String content = "";
-        try {
-            URL url = Resources.getResource("login.jsp");
-            content = Resources.toString(url, StandardCharsets.UTF_8);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-        return content;
+    public Viewable login() {
+        return new Viewable("/login.jsp");
     }
 
     @GET
     @Path("/registro")
-    public String showRegistro() {
-        String content = "";
-        try {
-            URL url = Resources.getResource("registro.jsp");
-            content = Resources.toString(url, StandardCharsets.UTF_8);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-        return content;
+    public Viewable registro() {
+        return new Viewable("/registro.jsp");
     }
-
 }
-
 
 

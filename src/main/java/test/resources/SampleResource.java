79251c4879abd469108abcd278d7c89ea04549c5
wyattpy <71204725+wyattpy@users.noreply.github.com>
2020-10-29 10:40:43 -0300

    aa

--- src/main/java/test/resources/SampleResource.java
+++ src/main/java/test/resources/SampleResource.java
@@ -1,29 +1,65 @@
 package test.resources;
 
-import org.glassfish.jersey.server.mvc.Viewable;
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class SampleResource {
 
     @GET
-    @Path("")
-    public Viewable index() {
-        return new Viewable("resources/index.html");
+    @Path("/")
+    @Produces(MediaType.TEXT_HTML)
+    public String showHellowWorld() {
+        String content = "";
+        try {
+            URL url = Resources.getResource("index.html");
+            content = Resources.toString(url, StandardCharsets.UTF_8);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return content;
     }
 
     @GET
     @Path("/login")
-    public Viewable login() {
-        return new Viewable("resources/login.jsp");
-    }
+    @Produces(MediaType.TEXT_HTML)
+    public String showLogin() {
+        String content = "";
+        try {
+            URL url = Resources.getResource("login.html");
+            content = Resources.toString(url, StandardCharsets.UTF_8);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
 
+        return content;
+    }
     @GET
     @Path("/registro")
-    public Viewable registro() {
-        return new Viewable("resources/registro.jsp");
+    @Produces(MediaType.TEXT_HTML)
+    public String showRegistro() {
+        String content = "";
+        try {
+            URL url = Resources.getResource("registro.html");
+            content = Resources.toString(url, StandardCharsets.UTF_8);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return content;
     }
+
 }
+
+
 
 
